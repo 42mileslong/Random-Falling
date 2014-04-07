545be1e35724ff79c3926b1c3120fa00099ff4e7
Rafi Long <rafilong42@gmail.com>
2014-04-07 12:02:37 -0700

    Tried to debug

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -5,28 +5,31 @@ public class Main {
 	public static int gameHeight = 22;
 	public static int gameWidth = input();
 	public static Square[][] grid = new Square[gameWidth][gameHeight];
-	
+
 	private static int currentRuns = 0;
 	public static boolean finishedRun = false;
 	private static int droppedSquares = 0;
 
 	public static void main(String args[]) {
 		initialize();
-		
-		while (currentRuns < 100) {
+
+		while (currentRuns < 1) {
 			while (!finishedRun) {
 				Square.spawnNew();
 				Square.dropAll();
 				droppedSquares++;
+				checkClearLine();
+				printGrid();
+				System.out.println();
 			}
 			currentRuns++;
 			System.out.println("Run " + currentRuns + " count is " + droppedSquares);
-			finishedRun = true;
+			finishedRun = false;
 			droppedSquares = 0;
 			Square.clearGrid();
 		}
 	}
-	
+
 	private static void initialize() {
 		for (int x = 0; x < gameWidth; x++) {
 			for (int y = 0; y < Main.gameHeight; y++) {
@@ -39,7 +42,7 @@ public class Main {
 			}
 		}
 	}
-		
+
 	private static int input() {
 		System.out.println("What is the width of the gameboard?");
 		Scanner sc = new Scanner(System.in);
@@ -51,5 +54,35 @@ public class Main {
 			System.err.println("Enter a valid integer");
 			return input();
 		}
+	}
+	
+	private static void printGrid() {
+		for (int y = gameHeight -1; y >= 0; y--) {
+			for (int x = 0; x < Main.gameWidth; x++) {
+//				System.out.println(grid[x][y].current);
+				if (grid[x][y].current) System.out.print("1 ");
+				if (!grid[x][y].current) System.out.print("0 "); 
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void checkClearLine() {
+		boolean allAlive = true;
+		for (int x = 0; x < gameWidth; x++) {
+			if (grid[x][0].current) {
+				allAlive = false;
+			}
+		}
+		if (allAlive = true) { 
+			clearLine(0);
+		}
+	}
+	
+	private static void clearLine(int y) {
+		for (int x = 0; x < gameWidth; x++) {
+			grid[x][y].current = false;
+		}
+		Square.dropAll();
 	}
 }
--- Tetris/src/Square.java
+++ Tetris/src/Square.java
@@ -4,13 +4,13 @@ public class Square {
 	public int x;
 	public int y;
 	public Square below;
-	
+
 	public Square(int x, int y) {
 		this.current = false;
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public static void dropAll() {
 		for (int x = 0; x < Main.gameWidth; x++) {
 			for (int y = 0; y < Main.gameHeight; y++) {
@@ -18,30 +18,18 @@ public class Square {
 			}
 		}
 	}
-	
+
 	private void drop() {
-		boolean noneBelow = false;
-		while (!noneBelow) {
-			if (this.y > 0 && !this.below.current) {
-				this.current = false;
-				this.below.current = true;
-				this.resetBelow();
-			} else {
-				noneBelow = true;
-			}
+		if (this.y > 0 && !this.below.current) {
+			this.current = false;
+			this.below.current = true;
+			this.below.drop();
 		}
 	}
-		
-	private void resetBelow() {
-		if (this.y > 0) {
-			this.below = Main.grid[x][y-1];
-		} else {
-			this.below = null;
-		}
-	}
-	
+
 	public static void spawnNew() {
         int ranX = (int) (Math.random() * Main.gameWidth);
+        System.out.println("Random x is " + ranX);
         if (!Main.grid[ranX][Main.gameHeight -1].current) {
         	Main.grid[ranX][Main.gameHeight -1].current = true;
         	Main.grid[ranX][Main.gameHeight -1].drop();
@@ -49,7 +37,7 @@ public class Square {
         	Main.finishedRun = true;
         }
 	}
-	
+
 	public static void clearGrid() {
 		for (int x = 0; x < Main.gameWidth; x++) {
 			for (int y = 0; y < Main.gameHeight; y++) {
