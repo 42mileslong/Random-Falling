fffca2cf5dc6bae5d5550999491bccadcc0c991c
Rafi Long <rafilong42@gmail.com>
2014-04-22 10:32:07 -0700

    Removed pointless commenting

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -32,108 +32,99 @@ import java.util.Scanner;
 
 public class Main {
 	/* Grid variables */
-	public static int gameHeight = 22; //contains the game height (22), the number of tiles spanning the height of the grid
-	public static int gameWidth = input("What is the width of the gameboard?"); //contains the game width, the number of tiles spanning the width of the grid, by calling input()
-	public static Square[][] grid = new Square[gameWidth][gameHeight]; //makes a grid with the width and height of above variables
+	public static int gameHeight = 22;
+	public static int gameWidth = input("What is the width of the gameboard?");
+	public static Square[][] grid = new Square[gameWidth][gameHeight];
 
 	/* Calculation variables */
-	private static int totalRuns = 5; //stores the total amount of runs
-	public static boolean finishedRun = false; //stores whether the current run has finished (set by the method Square.spawnNew() when it tries to spawn a square in a tile that already contains a square)
-	private static int droppedSquares = 0; //stores the amount of squares dropped (increased whenever a new block is spawned)
-	private static ArrayList<Integer> droppedSquaresList = new ArrayList<Integer>(); //stores the amount of squares dropped for all of the runs
+	private static int totalRuns = 5;
+	public static boolean finishedRun = false;
+	private static int droppedSquares = 0;
+	private static ArrayList<Integer> droppedSquaresList = new ArrayList<Integer>();
 
 	public static void main(String args[]) {
-		initialize(); //initializes the grid
-		System.out.println("Run counts are:"); //tells the user that the below numbers are the number of squares dropped for each run
+		initialize();
+		System.out.println("Run counts are:");
 		
-		for (int i = 0; i < totalRuns; i++) { //this loops until there have been the same number of runs as the variable totalRuns
-			while (!finishedRun) { //this loops until Square.spawnNew() stops it when it spawns a square in a tile containing a square
-				Square.spawnNew(); //spawns a new square
-				Square.dropAll(); //checks to drop all squares, but the only square effected is the new square
-				droppedSquares++; //adds a new square to the list of dropped squares
-				checkClearLine(); //checks to see whether a line needs to be cleared
-//				printGrid(); //printGrid prints the current grid, which is useful for testing but not needed now
+		for (int i = 0; i < totalRuns; i++) {
+			while (!finishedRun) {
+				Square.spawnNew();
+				Square.dropAll();
+				droppedSquares++;
+				checkClearLine();
 			}
-			//once the run finishes...
-			System.out.println(droppedSquares); //prints the number of dropped squares
-			droppedSquaresList.add(droppedSquares); //adds the number of dropped squares to the array droppedSquaresList
-			finishedRun = false; //resets finishedRun
-			droppedSquares = 0; //resets the number of dropped squares
-			Square.clearGrid(); //clears the grid, getting ready for a new run
+			System.out.println(droppedSquares);
+			droppedSquaresList.add(droppedSquares);
+			finishedRun = false;
+			droppedSquares = 0;
+			Square.clearGrid();
 		}
-		//after the runs are finished...
-		System.out.println("Average dropped squares are " + findAverage()); //prints the average squares dropped with findAverage()
+		System.out.println("Average dropped squares are " + findAverage());
 	}
 
-	//initializes by putting squares in the grid
 	private static void initialize() {
-		for (int x = 0; x < gameWidth; x++) { //loops for width
-			for (int y = 0; y < Main.gameHeight; y++) { //loops for height
-				grid[x][y] = new Square(x, y); //makes a new square in the current xy
-				if (y > 0) { //if the y is above the bottom
-					grid[x][y].below = grid[x][y-1]; //sets the square below to be the `below` square
-				} else { //if the y is on the bottom
-					grid[x][y].below = null; //it sets the `below` to be null (there is no below)
+		for (int x = 0; x < gameWidth; x++) {
+			for (int y = 0; y < Main.gameHeight; y++) {
+				grid[x][y] = new Square(x, y);
+				if (y > 0) {
+					grid[x][y].below = grid[x][y-1];
+				} else {
+					grid[x][y].below = null;
 				}
 			}
 		}
 	}
 
-	//gets user input and asks a certain question
 	private static int input(String question) {
-		System.out.println(question); //prints a question to the user
-		Scanner sc = new Scanner(System.in); //makes a new scanner
-		String text = sc.nextLine(); //retrieves the text before the new line
-		sc.close(); //closes the scanner
-		try { //try catch for testing whether the string is a valid integer
-			return Integer.parseInt(text); //tries to parse the string to turn it into an integer
-		} catch (NumberFormatException e) { //if the string is not a integer...
-			System.err.println("Enter a valid integer"); //tells the user to enter a valid integer
-			return input(question); //repeats the cycle
+		System.out.println(question);
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+		sc.close();
+		try {
+			return Integer.parseInt(text);
+		} catch (NumberFormatException e) {
+			System.err.println("Enter a valid integer");
+			return input(question);
 		}
 	}
-	
-	//checks to see whether the bottom line is ready to be cleared, and because only one square is dropping at a time it doesn't have to check all of the lines
+
 	private static void checkClearLine() {
-		boolean allAlive = true; //a variable that assumes there are squares in all the tiles in the line
-		for (int x = 0; x < gameWidth; x++) { //loops for gameWidth
-			if (!grid[x][0].active) { //if there is not a square in the x
-				allAlive = false; //sets all alive to false
+		boolean allAlive = true;
+		for (int x = 0; x < gameWidth; x++) {
+			if (!grid[x][0].active) {
+				allAlive = false;
 			}
 		}
-		if (allAlive) { //if allAlive was never set to false
-			clearLine(0); //it calls clearLine() for the line of 0 (the bottom)
+		if (allAlive) {
+			clearLine(0);
 		}
 	}
-	
-	//removes square from a current line and drops the rest
+
 	private static void clearLine(int y) {
-		for (int x = 0; x < gameWidth; x++) { //loops for gameWidth
-			grid[x][y].active = false; //sets the square in the current position to false, indicating that there is no square there, and only an empty tile
+		for (int x = 0; x < gameWidth; x++) {
+			grid[x][y].active = false;
 		}
-		Square.dropAll(); //drops all of the squares
+		Square.dropAll();
 	}
-	
-	//finds the average number of dropped squares
+
 	private static double findAverage() {
-		double total = 0; //a variable to hold the total of all dropped squares
-        for (Integer aDroppedSquaresList : droppedSquaresList) { //loops for the size of droppedSquaresList
-            total += aDroppedSquaresList; //adds the dropped square to total
+		double total = 0;
+        for (Integer aDroppedSquaresList : droppedSquaresList) {
+            total += aDroppedSquaresList;
         }
-		return (total/droppedSquaresList.size()); //returns total divided by the number of dropped squares
+		return (total/droppedSquaresList.size());
 	}
 	
 	
 	@SuppressWarnings("unused")
-	//prints the grid for testing purposes
 	private static void printGrid() {
-		for (int y = gameHeight -1; y >= 0; y--) { //inverted gameHeight loop, so lines printed will be in order
-			for (int x = 0; x < Main.gameWidth; x++) { //loops of gameWidth
-				if (grid[x][y].active) System.out.print("1 "); //if there is a square, it prints `1`
-				if (!grid[x][y].active) System.out.print("0 "); //if there is not a square, it prints `0`
+		for (int y = gameHeight -1; y >= 0; y--) {
+			for (int x = 0; x < Main.gameWidth; x++) {
+				if (grid[x][y].active) System.out.print("1 ");
+				if (!grid[x][y].active) System.out.print("0 ");
 			}
-			System.out.println(); //makes a new line (so each line will be on a new line)
+			System.out.println();
 		}
-		System.out.println(); //makes a space after the printed grid
+		System.out.println();
 	}
 }
--- Tetris/src/Square.java
+++ Tetris/src/Square.java
@@ -11,54 +11,50 @@
  */
 
 public class Square {
-	public boolean active; //a variable for holding whether there is a square in the tile
-	public int x; //a variable for holding the x
-	public int y; //a variable for holding the y
-	public Square below; //a pointer to the square in the tile beneath the current square
+    /* Instance Variables */
+	public boolean active;
+	public int x;
+	public int y;
+	public Square below;
 
-	//a constructor, with the arguments of the x and y
 	public Square(int x, int y) { 
-		this.active = false; //sets active to be false (there is no square there yet)
-		this.x = x; //sets x to the x argument
-		this.y = y; //sets y to the y argument
+		this.active = false;
+		this.x = x;
+		this.y = y;
 	}
 
-	//drops all of the squares by calling drop for every square
 	public static void dropAll() {
-		for (int x = 0; x < Main.gameWidth; x++) { //loops for gameWidth
-			for (int y = 0; y < Main.gameHeight; y++) { //loops for gameHeight
-				if (Main.grid[x][y].active) { //if there is a square in the `square`
-					Main.grid[x][y].drop(); //calls drop for the square
+		for (int x = 0; x < Main.gameWidth; x++) {
+			for (int y = 0; y < Main.gameHeight; y++) {
+				if (Main.grid[x][y].active) {
+					Main.grid[x][y].drop();
 				}
 			}
 		}
 	}
 
-	//tries to drop the square
 	public void drop() {
-		if (this.y > 0 && !this.below.active) { //if the tile below doesn't have a square
-			this.active = false; //sets the current square to be false
-			this.below.active = true; //sets the tile beneath the current square to be active
-			this.below.drop(); //tries to drop the below square
+		if (this.y > 0 && !this.below.active) {
+			this.active = false;
+			this.below.active = true;
+			this.below.drop();
 		}
 	}
 
-	//spawns a new square randomly on the top of the grid
 	public static void spawnNew() {
-        int ranX = (int) (Math.random() * Main.gameWidth); //sets the ranX variable to equal a random number between 0 and the gameWidth
-        if (!Main.grid[ranX][Main.gameHeight -1].active) { //if there isn't a square in the chosen tile
-        	Main.grid[ranX][Main.gameHeight -1].active = true; //sets the tile, and therefore the square, to be true
-        	Main.grid[ranX][Main.gameHeight -1].drop(); //tries to drop the square
-        } else { //if there is a square already in the tile when it tries to spawn a square
-        	Main.finishedRun = true; //sets the finishedRun to be true
+        int ranX = (int) (Math.random() * Main.gameWidth);
+        if (!Main.grid[ranX][Main.gameHeight -1].active) {
+        	Main.grid[ranX][Main.gameHeight -1].active = true;
+        	Main.grid[ranX][Main.gameHeight -1].drop();
+        } else {
+        	Main.finishedRun = true;
         }
 	}
 
-	//clears the grid (used after a run is finished
 	public static void clearGrid() {
-		for (int x = 0; x < Main.gameWidth; x++) { //for the gameWidth
-			for (int y = 0; y < Main.gameHeight; y++) { //for the gameHeight
-				Main.grid[x][y].active = false; //sets all squares.active to false
+		for (int x = 0; x < Main.gameWidth; x++) {
+			for (int y = 0; y < Main.gameHeight; y++) {
+				Main.grid[x][y].active = false;
 			}
 		}
 	}
