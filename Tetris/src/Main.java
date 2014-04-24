58baa0da8aa745491ae4b85ecdf191e2ad1bcff8
Rafi Long <rafilong42@gmail.com>
2014-04-24 10:31:36 -0700

    Moved commenting on project to top of Main

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
-
 /*
  * Random Falling
  * Coded by Rafi Long
  * Reviewed by Phil Long
- * 
+ *
  * This program is meant to calculate the average amount of squares dropped in a Tetris game if each gamepiece, or Tetrimo, was a single square.
  * However, this program allows you to change the gameboard width.
- * 
+ *
  * View the graphed data below:
  * https://docs.google.com/presentation/d/197B9tDgbns_cOALpqiPZdx58X_jv4NQK8UDJqL6JUAQ/edit?usp=sharing
- * 
+ *
  * Read about the classes at the top of each file.
  * As always, documentation of the code is in the code.
  * Thank you for checking out my code! I hope you have fun!
  */
-
 /*
  * Terms:
  * Grid - A grid of `tiles` (see below) of height 22 and variable width.
@@ -23,6 +20,9 @@ import java.util.Scanner;
  * Square - A space on the `grid` (see above) that is occupied. In other words, an active `tile`.
  * Run - What would be called a tetris `game`. A cycle, started by the spawning of one block and ending with one column of `squares` (see above) reaching the top.
  */
+
+import java.util.Scanner;
+
 
 /**
  * Performs calculations and interprets user input
